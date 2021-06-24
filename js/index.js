50b245a5f4c2419a21dccba216a4b97ffcf27c89
Madeline Fisher <61165851+madFish2020@users.noreply.github.com>
2021-06-24 15:59:25 -0500

    set up interactions sort of

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 console.log("Your index.js file is loaded correctly!");
+
+$(".button").on('click', function(){
+    console.log('button clicked')
+});
+
+$('.lab-info').on('click', function(){
+    console.log('lab clicked')
+})
+$(".top").on('click', function(){
+    console.log('nav clicked')
+});
+
