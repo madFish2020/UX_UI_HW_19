942da6547e0859a2c2a76bd0d8cf80cbb76a9f15
Madeline Fisher <61165851+madFish2020@users.noreply.github.com>
2021-06-24 16:20:17 -0500

    button interactions styled

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 console.log("Your index.js file is loaded correctly!");
 
-$(".button").on('click', function(){
-    console.log('button clicked')
+$(".button").on('mouseover',function(){
+    this.style.backgroundColor ='#345C66'
+}).on('mouseout', function(){
+    this.style.backgroundColor ='#608790'
 });
 
 $('.lab-info').on('click', function(){
